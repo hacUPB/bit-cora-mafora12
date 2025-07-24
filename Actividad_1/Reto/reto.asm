9716a496c8d5cba267800d3370987f4f5b7dcdf1
mafora12 <marianaflorez1224@gmail.com>
2025-07-24 09:08:13 -0500

    Fn_2

--- Actividad_1/Reto/reto.asm
+++ Actividad_1/Reto/reto.asm
@@ -125,6 +125,47 @@ M=M+1
 0;JMP
 (END)
 
+
+
+//Punto 16
+@0
+D=A
+@sum
+M=D
+
+@0
+D=A
+@j
+M=D
+
+(LOOP_START)
+@j
+D=M 
+@10
+D=D-A
+@END
+D;JGE
+
+@arr
+D=A
+@j
+A=D+M
+D=M
+
+@sum
+M=D+M
+
+@j 
+M=M++1
+
+@LOOP_START
+0;JMP
+
+(END)
+@END
+0;JMP
+
+
 // Punto 17
 
 @7 
